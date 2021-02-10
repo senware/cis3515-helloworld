a6627b5a420993f03b4ac7069a844568d01e2718
Sen <tug81999@temple.edu>
2021-02-09 20:35:33 -0500

    Added button on click behavior

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.helloButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+               TextView hText = (TextView) findViewById(R.id.helloText);
+               hText.setText("You clicked the button.");
+            }
+        });
+
+
     }
 }
